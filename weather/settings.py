7b43c9add92a3d724bc01f7689a68be902fa53c2
Simeon Akanbi <simeonakanbi99@gmail.com>
2022-02-08 22:07:57 +0100

    Changed the directory for static files

--- weather/settings.py
+++ weather/settings.py
@@ -119,8 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'weather/Static'
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'weather/static/',
+    BASE_DIR / 'Main/static/',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
