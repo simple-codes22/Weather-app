4908518a962f2487a0107943778186a4c40c70ab
Simeon Akanbi <simeonakanbi99@gmail.com>
2022-02-08 21:57:14 +0100

    Changed the static directory

--- weather/settings.py
+++ weather/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -121,7 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'Main/static',
+    BASE_DIR / 'weather/static/',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
