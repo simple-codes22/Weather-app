54183603478e53e0afc768211202e690e1b91135
Simeon Akanbi <simeonakanbi99@gmail.com>
2022-04-20 14:48:26 +0100

    Changed the debug status

--- weather/settings.py
+++ weather/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qx08lau*5vqf(vwfr1yec18g%2@+==9$hi!b!^p7o(^#yiocoa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
